51e7007a61e9da88e3961a9334c3269feb92ad7e
1-aditya-2 <99257070+1-aditya-2@users.noreply.github.com>
2023-12-11 00:33:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import 'swiper/css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Header from './pages/Header';
+import Banner from './pages/Banner';
+import Main from './pages/Main';
+import Footer from './pages/Footer';
+
+function App() {
+  return (
+    <>
+     <Header/>
+     <Banner/>
+     <Main/>
+     <Footer/>
+    </>
+  );
+}
+
+export default App;
