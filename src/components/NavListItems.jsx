b198ffef5f38e6e8530d68313baf31f9d355484a
1-aditya-2 <99257070+1-aditya-2@users.noreply.github.com>
2023-12-11 00:32:53 +0530

    initial commit

---
+++ src/components/NavListItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './navListItems.css'
+function NavListItems({nav}){
+    return(
+        <li>
+            <a href={nav.link}>{nav.name}</a>
+        </li> 
+    );
+}
+
+export default NavListItems;
